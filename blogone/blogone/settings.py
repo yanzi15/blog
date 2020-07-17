25bce3510c71e15393aa89e4b65ccd48569c7a1c
yanzi15 <1935531489@qq.com>
2020-07-17 22:02:54 +0800

    '数据库配置'

--- blogone/blogone/settings.py
+++ blogone/blogone/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'blogone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogone',
+        'USER':'xiaoyan',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
